f1a1d5a46485981075ca6d034775ea14d434237c
alanfelipealiske@gmail.com <alanfelipealiske@gmail.com>
2022-04-04 08:19:58 -0300

    Update

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular1';
+  nome = 'Alan Felipe'
+  nomes = ['alan', 'Amanda', 'aliske', 'josé']
+}
